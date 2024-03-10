4c7cf3e7eaf1941c3fe878f75e3a49aec78259d0
l4t3b0 <l4t3b0@gmail.com>
2024-03-10 22:11:47 +0100

    a

---
+++ crontab-helper.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+crontab_add() {
+  local cron_expr=$1
+  local cron_command=$2
+  local tmp_file=/tmp/crontab.tmp
+
+  case "$(echo "${cron_expr}" | tr '[:lower:]' '[:upper:]')" in
+    *@YEARLY* ) echo "INFO: Cron expression ${cron_expr} re-written to 0 0 1 1 *" && cron_expr="0 0 1 1 *";;
+    *@ANNUALLY* ) echo "INFO: Cron expression ${CRON_EXPR} re-written to 0 0 1 1 *" && cron_expr="0 0 1 1 *";;
+    *@MONTHLY* ) echo "INFO: Cron expression ${cron_expr} re-written to 0 0 1 * *" && cron_expr="0 0 1 * * ";;
+    *@WEEKLY* ) echo "INFO: Cron expression ${cron_expr} re-written to 0 0 * * 0" && cron_expr="0 0 * * 0";;
+    *@DAILY* ) echo "INFO: Cron expression ${cron_expr} re-written to 0 0 * * *" && cron_expr="0 0 * * *";;
+    *@MIDNIGHT* ) echo "INFO: Cron expression ${cron_expr} re-written to 0 0 * * *" && cron_expr="0 0 * * *";;
+    *@HOURLY* ) echo "INFO: Cron expression ${cron_expr} re-written to 0 * * * *" && cron_expr="0 * * * *";;
+    *@* ) echo "WARNING: Cron expression ${cron_expr} is not supported. Stopping." && exit 1;;
+  esac
+
+  echo "${cron_expr} ${cron_command}" > ${tmp_file}
+  echo "DEBUG: adding the following line to the crontab: `cat ${tmp_file}`;"
+  crontab ${tmp_file}
+
+  rm ${tmp_file}
+}
+
+crond_start() {
+  rm -f /tmp/sync.log
+  rm -f /tmp/crond.log
+
+  touch /tmp/sync.log
+  touch /tmp/crond.log
+
+  echo "INFO: Starting crond ..."
+  crond -b -l 0 -L /tmp/crond.log
+  echo "INFO: crond started"
+}
+
+crond_tail() {
+  tail -F /tmp/crond.log /tmp/sync.log
+}
