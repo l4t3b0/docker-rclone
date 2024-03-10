df85fc5f0bb525edea98a53fd97196adc13be5dc
l4t3b0 <l4t3b0@gmail.com>
2024-03-10 23:29:10 +0100

    a

--- healthchecks.io.sh
+++ healthchecks.io.sh
@@ -8,7 +8,7 @@ healthchecks_io_start() {
     url=${HEALTHCHECKS_IO_URL}/start
     echo "INFO: Sending helatchecks.io start signal to '${url}'"
 
-    wget ${url} -O /dev/null
+    wget ${url} -O /dev/null -q -S 2>&1 |head -n 1
   fi
 }
 
@@ -28,6 +28,6 @@ healthchecks_io_end() {
       echo "WARNING: Sending helatchecks.io failure signal to '${url}'"
     fi
 
-    wget ${url} -O /dev/null
+    wget ${url} -O /dev/null -q -S 2>&1 |head -n 1
   fi
 }
